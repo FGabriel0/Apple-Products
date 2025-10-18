b1740218156e8ca494bc53d66c0a7c1d8f9ed941
FGabriel0 <91754983+FGabriel0@users.noreply.github.com>
2025-10-18 19:07:52 -0300

    Criação da Home

--- apple-products/src/App.jsx
+++ apple-products/src/App.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Controls from './components/Controls'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Iphone from './components/Iphone'
+import PageTransition from "./components/PageTransition"
 
 const App = () => {
+  const[frameZoom, setFrameZoom]= useState(false)
+  const [activePage, setActivePage] = useState(0)
+
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex)
+  }
+
+  const toggleZoom = () => {
+  
+      setFrameZoom(!frameZoom)
+    
+  }
+
   return (
-    <div>App</div>
+    <div className="w-full h-screen grid place-items-center">
+      <div
+        className={`${
+          frameZoom && 'min-w-[97vw] min-h-[97vh]'
+        } w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 flex`}
+      >
+        <Navbar
+          activePage={activePage}
+          handleNavClick={handleNavClick}
+          />
+        <Controls toggleZoom={toggleZoom}
+         frameZoom={frameZoom}
+         />
+
+          <div className="flex-grow">
+            <PageTransition activePage={activePage}>
+              <Home onNavigate={handleNavClick}/>
+              <Iphone/>
+            </PageTransition>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
